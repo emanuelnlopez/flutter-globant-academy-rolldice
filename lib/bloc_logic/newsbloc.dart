5c9cd23a842fc646eba144d4d1d66d47e286fd90
Aecio Lima <aeclean@gmail.com>
2024-10-31 10:28:23 -0300

    create the news bloc to fetch the articles

---
+++ lib/bloc_logic/newsbloc.dart
@@ -0,0 +1,23 @@
+import 'package:bloc/bloc.dart';
+import 'package:rolldice/bloc_logic/news_events.dart';
+import 'package:rolldice/bloc_logic/news_state.dart';
+import 'package:rolldice/service/api_service.dart';
+
+class NewsBloc extends Bloc<NewsEvent, NewsState> {
+  NewsBloc() : super(IdleState()) {
+    on(fetchArticles);
+  }
+
+  ApiService service = ApiService();
+
+  Future<void> fetchArticles(NewsEvent event, Emitter<NewsState> state) async {
+    emit(LoadingState());
+
+    try {
+      var response = await service.fetchArticles();
+      emit(SuccessState(response));
+    } catch (e) {
+      emit(ErrorState(e));
+    }
+  }
+}
