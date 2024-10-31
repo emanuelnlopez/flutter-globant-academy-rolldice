c80d661927a59cd377e0ab77036d184281902efd
Aecio Lima <aeclean@gmail.com>
2024-10-31 10:28:42 -0300

    implements the service class

---
+++ lib/service/api_service.dart
@@ -0,0 +1,16 @@
+import 'package:dio/dio.dart';
+import 'package:rolldice/news_model.dart';
+
+class ApiService {
+  final dio = Dio();
+
+  Future<List<NewsModel>> fetchArticles() async {
+    try {
+      dynamic response = await dio.get(
+          "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=56537b45c1384e46a13bc1a260de5333");
+      return NewsModel.fromList((response as Response).data["articles"]);
+    } on Exception catch (e) {
+      return [];
+    }
+  }
+}
